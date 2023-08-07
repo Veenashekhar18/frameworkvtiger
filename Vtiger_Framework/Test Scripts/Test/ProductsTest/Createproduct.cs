11dfd20e083644e5a9b32d25c168fed2e79e93e8
Veenashekhar18 <veenaraichur58@gmail.com>
2023-08-07 10:24:12 +0530

    create product

---
+++ Vtiger_Framework/Test Scripts/Test/ProductsTest/Createproduct.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vtiger_Framework.Main_Folder.ObjectRepository.ProductsRepo;
+
+namespace Vtiger_Framework.Test_Scripts.Test.ProductsTest
+{
+    [TestClass]
+    internal class Createproduct 
+    {
+        [TestMethod]
+
+        public static void Create()
+        {
+            
+
+
+        }
+    }
+}
