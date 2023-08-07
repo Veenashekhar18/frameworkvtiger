8cc341e9a0ff271602ccb1b3aca7699a89f6eee7
Admin <Admin@DESKTOP-HRKOFKI>
2023-08-07 14:40:08 +0530

    new campaign

--- Vtiger_Framework/Test Scripts/Test/CampaignsTest/CreateNewCampaign.cs
+++ Vtiger_Framework/Test Scripts/Test/CampaignsTest/CreateNewCampaign.cs
@@ -20,22 +20,38 @@ namespace Vtiger_Framework.Test_Scripts.Test.CampaignsTest
         [Owner("Thejas")]
         public void Create_New_campaign()
         {
-            IWebDriver driver=new ChromeDriver();
+            //git checking
+            // IWebDriver driver = new ChromeDriver();
             LoginPage Login = new LoginPage(driver);
             VTigerHomePage home = new VTigerHomePage(driver);
+            CampaignsHomePage campaignHome = new CampaignsHomePage(driver);
+            CreatingNewCampaignPage createCampaign = new CreatingNewCampaignPage(driver);
             WebDriverUtility webaction = new WebDriverUtility();
             webaction.WaitImplicitly(driver);
             webaction.MaximiseWindow(driver);
             driver.Navigate().GoToUrl("http://localhost:8888/");
             Login.Login();
-
-
-          
             home.MoreDropDown();
             home.CampaignLink();
-           
+            campaignHome.click_on_add();
 
-            
+            //creating the new campaign
+            string campaignName = "Mi Mobiles";
+            string campaignType = "Advertisement";
+            string campaignStatus = "Active";
+            string revenue = "2000000";
+            string closeDate = "10/20/2023";
+            createCampaign.CreateNewCampaign_Test(campaignName, campaignType, campaignStatus, revenue, closeDate);
+            campaignHome.campaign_Link.Click();
+
+            //validating and creating extent report.
+            string path = "C:\\Users\\Admin\\Source\\Repos\\Vtiger_Framework_new\\Vtiger_Framework\\Results\\Extent_Report_Thejas\\";
+            ExtentReports extentReports = new ExtentReports();
+            ExtentHtmlReporter htmlReport = new ExtentHtmlReporter(path);
+            extentReports.AttachReporter(htmlReport);
+            ExtentTest test = extentReports.CreateTest("Create_New_Campaign");
+
+
             Thread.Sleep(2000);
 
 
